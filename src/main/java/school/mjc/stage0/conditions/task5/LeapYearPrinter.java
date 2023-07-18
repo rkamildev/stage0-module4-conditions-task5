a4e7b4e633993d767f129eefdb1536b3fa372b99
rkamildev <rkamil.int@gmail.com>
2023-07-18 11:17:59 +0500

    Switch prac

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        switch (year){
+            case 2000:
+            case 2004:
+                System.out.println("leap");
+                break;
+            case 1900:
+                System.out.println("not leap");
+                break;
+        }
     }
 }
